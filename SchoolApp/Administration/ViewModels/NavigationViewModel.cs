aa641ca9d5d07a21dcc328b93d5b44200c6fe69f
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-10-06 22:09:38 -0500

    Add files via upload

--- SchoolApp/Administration/ViewModels/NavigationViewModel.cs
+++ SchoolApp/Administration/ViewModels/NavigationViewModel.cs
@@ -106,42 +106,6 @@ namespace Administration.ViewModels
         
         #endregion
 
-        //Do not remove the code below
-        #region ICommand
-        public class BaseCommand : ICommand
-        {
-            private Predicate<object> _canExecute;
-            private Action<object> _method;
-            public event EventHandler CanExecuteChanged;
-
-            public BaseCommand(Action<object> method)
-                : this(method, null)
-            {
-            }
-
-            public BaseCommand(Action<object> method, Predicate<object> canExecute)
-            {
-                _method = method;
-                _canExecute = canExecute;
-            }
-
-            public bool CanExecute(object parameter)
-            {
-                if (_canExecute == null)
-                {
-                    return true;
-                }
-
-                return _canExecute(parameter);
-            }
-
-            public void Execute(object parameter)
-            {
-                _method.Invoke(parameter);
-            }
-        }
-        #endregion
-
         public class MainWindowViewModel
         {
             public void ExitApp()
