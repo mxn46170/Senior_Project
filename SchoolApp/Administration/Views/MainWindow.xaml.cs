c18e9fc9632c44a953f9536d5a99bc67d161c654
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-10-13 12:12:16 -0500

    Add files via upload

--- SchoolApp/Administration/Views/MainWindow.xaml.cs
+++ SchoolApp/Administration/Views/MainWindow.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static Administration.ViewModels.NavigationViewModel;
 
 namespace Administration
 {
@@ -24,19 +23,17 @@ namespace Administration
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly NavigationViewModel nav = new NavigationViewModel();
         private readonly MainWindowViewModel mvm = new MainWindowViewModel();
         public MainWindow()
         {
             Thread.Sleep(1500);
             InitializeComponent();
-            DataContext = nav;
+            DataContext = mvm;
         }
 
         private void ShutDown(object sender, RoutedEventArgs e)
         {
             mvm.ExitApp();
-            //Application.Current.Shutdown();
             //check if the current screen has any unsaved data -> output message to save or not?
         }
 
@@ -51,5 +48,53 @@ namespace Administration
         {
             mvm.LogOut();
         }
+
+        #region Navigation
+        private void DisplaySchoolInfoView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new SchoolSetupViewModel();
+        }
+
+        private void DisplaySchoolYearView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new SchoolYearViewModel();
+        }
+
+        private void DisplayBuildingRoomsView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new BuildingsSetupViewModel();
+        }
+
+        private void DisplayDepartmentsView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new DepartmentsSetupViewModel();
+        }
+
+        private void DisplayMajorsView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MajorMinorViewModel();
+        }
+
+        private void DisplayProfessorsView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new ProfessorsViewModel();
+        }
+
+        private void DisplayCoursesView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CoursesViewModel();
+        }
+
+        private void DisplayStudentsView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new StudentsViewModel();
+        }
+
+        private void DisplayEventsView(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new EventsViewModel();
+        }
+
+        #endregion
     }
 }
--- SchoolApp/Administration/Views/StudentsView.xaml.cs
+++ SchoolApp/Administration/Views/StudentsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Administration.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace Administration.Views
         public StudentsView()
         {
             InitializeComponent();
+        }
+
+        private void DisplayAddEditStudentsView(object sender, RoutedEventArgs e)
+        {
+            StudentMain.Content = new AddEditStudentsViewModel();
         }
     }
 }
