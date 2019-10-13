4ebb0523b2a72a8885339e8a1df1a8f9c72eaf6b
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-10-13 12:21:19 -0500

    Add files via upload

---
+++ SchoolApp/SchoolApp_Student/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp_Student.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+        public MainWindowViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+
+        #region Public Methods
+
+
+        #endregion
+
+        #region Private Methods
+
+
+        #endregion
+    }
+}
