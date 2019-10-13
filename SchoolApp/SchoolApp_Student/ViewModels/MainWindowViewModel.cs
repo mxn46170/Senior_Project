1c783da877dce8c0dd5e2690e6018715c936bd23
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-10-13 12:33:27 -0500

    Add files via upload

--- SchoolApp/SchoolApp_Student/ViewModels/MainWindowViewModel.cs
+++ SchoolApp/SchoolApp_Student/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SchoolApp_Student.ViewModels
 {
@@ -25,6 +27,26 @@ namespace SchoolApp_Student.ViewModels
 
         #region Public Methods
 
+        public void ExitApp()
+        {
+            var result = MessageBox.Show("Are you sure you want to exit?", "Exiting", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+            return;
+        }
+
+        public void LogOut()
+        {
+            var result = MessageBox.Show("Are you sure you want to Log out?", "Logging out", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                Process.Start("UserLogin.exe");
+                Application.Current.Shutdown();
+            }
+            return;
+        }
 
         #endregion
 
