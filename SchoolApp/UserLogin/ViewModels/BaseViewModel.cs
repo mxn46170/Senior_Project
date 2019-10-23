692209a9d643317d41221d18a27445c8e6f457f1
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-10-23 15:04:17 -0500

    Add files via upload

---
+++ SchoolApp/UserLogin/ViewModels/BaseViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin.ViewModels
+{
+    public class BaseViewModel
+    {
+        #region Protected Methods
+        protected void SetupDataDirectory()
+        {
+            string _databaseLocation;
+            _databaseLocation = @"SchoolApp";
+            var path = Path.GetFullPath(_databaseLocation);
+            var temp = path.Substring(0, path.Length - 30);
+            var temp1 = temp + "\\SchoolU_Database";
+            AppDomain.CurrentDomain.SetData("DataDirectory", temp1);
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void propertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ SchoolApp/UserLogin/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,99 @@
+﻿using SchoolU_Database;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UserLogin.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        #region Contructors
+        public MainWindowViewModel()
+        {
+            SetupDataDirectory();
+        }
+
+        #endregion
+
+        #region Private Fields
+        private string _username;
+        private string _password;
+
+        #endregion
+
+        #region Public Properties
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                propertyChanged(nameof(UserName));
+            }
+        }
+
+        public string Password
+        {
+            private get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                propertyChanged(nameof(Password));
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void ValidateUserLogin(string pwd)
+        {
+            try
+            {
+                using (var context = new SchoolU_DBEntities())
+                {
+                    context.Database.Connection.Open();
+                    var admin = context.Admins.Where(i => i.Username == UserName && i.Password == pwd).SingleOrDefault();
+                    var student = context.Students.Where(i => i.StudentUserName == UserName && i.StudentPassword == pwd).SingleOrDefault();
+                    var professor = context.Professors.Where(i => i.ProfessorUserName == UserName && i.ProfessorPassword == pwd).SingleOrDefault();
+                    if (admin != null)
+                    {
+                        Process.Start("Administration.exe");
+                    }
+                    else if (student != null)
+                    {
+                        Process.Start("SchoolApp_Student.exe");
+                    }
+                    else if (professor != null)
+                    {
+                        Process.Start("SchoolApp_Professor.exe");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect username or password", "Failed Login", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    }
+                    Application.Current.Shutdown();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Application.Current.Shutdown();
+                return;
+            }
+        }
+
+        #endregion
+
+    }
+}
