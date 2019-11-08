c89f3d63a2cd79ddb7b4a013ec5aa054a6728209
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-11-08 16:59:40 -0600

    Add files via upload

---
+++ SchoolApp/SchoolApp_Professor/ViewModels/ProfessorsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration.ViewModels
+{
+    public class ProfessorsViewModel : BaseViewModel
+    {
+
+        public void ValidateUserLogin(string pwd)
+        {
+
+        }
+
+    }
+}
