7d7a9d03a8504e07c6156a15ec937b079aef13dd
mxn46170 <47306179+mxn46170@users.noreply.github.com>
2019-12-08 18:21:39 -0600

    Add files via upload

---
+++ SchoolApp/Administration/Models/StudentInformationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration.ViewModels
+{
+    public class StudentInformationModel : BaseViewModel
+    {
+        public string SFirstName { get; set; }
+
+        public string SLastName { get; set; }
+
+        public string SMajor { get; set; }
+ 
+        public string SstudentYear { get; set; }
+
+    }
+}
